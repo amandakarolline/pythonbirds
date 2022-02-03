4e8da12de7d395c21027f9ae9c004881c07b46b6
amandakarolline <amandakarolline@hotmail.com>
2022-02-03 14:28:14 -0300

    Criado atributo de classe olhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,7 @@
 class Pessoa:
+    #atributo default ou atributo de classe
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -22,6 +25,15 @@ if __name__ == '__main__':
     luciano.sobrenome = 'Ramalho'
     #removendo atributo dinamicamente, vai ser removido apenas do objeto luciano
     del luciano.filhos
+    #mudano o valor do atributo olhos apenas para luciano
+    luciano.olhos = 1
+    #apagando o atributo olhos apenas para luciano, mas permanece na classe
+    del luciano.olhos
     #verificando quais os atributos existem para cada um
     print(luciano.__dict__)
     print(renzo.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
